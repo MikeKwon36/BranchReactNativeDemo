eac7d882f3e7e162628bcfc927392cbea6683024
mikekwon36 <mkwon@branch.io>
2018-04-29 13:59:28 -0400

    test

---
+++ react-native-branch-deep-linking-master/examples/webview_example_native_ios/WebViewExample/WebViewExample-BridgingHeader.h
@@ -0,0 +1,9 @@
+//
+//  WebViewExample-BridgingHeader.h
+//  WebViewExample
+//
+//  Created by Jimmy Dee on 4/5/17.
+//  Copyright © 2017 Branch Metrics. All rights reserved.
+//
+
+#import "RCTSwiftLog.h"
